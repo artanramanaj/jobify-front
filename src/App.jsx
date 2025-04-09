95fcf5f487d41fccfe6549741962fe46100cf40c
Artan Rramanaj <artan.ramanaj@finnie.consulting>
2025-04-09 23:27:52 +0200

    creating the pages and exporting them as default from index file in page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomeLayout, Register, Login, Error } from "./pages";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 const App = () => {
-  return <h1>App</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
