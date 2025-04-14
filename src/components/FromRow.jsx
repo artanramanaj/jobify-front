e983cc8b945a1780bc664992589f5ff9d2865d80
Artan Rramanaj <artan.ramanaj@finnie.consulting>
2025-04-14 15:42:53 +0200

    register page

---
+++ src/components/FromRow.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FromRow = ({ type, name, defaultValue, labelText }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        className="form-input"
+        id={name}
+        name={name}
+        defaultValue={defaultValue || ""}
+        required
+      />
+    </div>
+  );
+};
+
+export default FromRow;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">go to login</Link>
-    </div>
+    <Wrapper>
+      <form action="" className="form">
+        <Logo />
+        <h4>Register </h4>
+        <FormRow type="text" name="name" defaultValue="john" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="Last Name"
+          defaultValue="doe"
+        />
+        <FormRow type="text" name="location" defaultValue="earth" />
+        <FormRow type="email" name="email" defaultValue="john.doe@gmail.com" />
+        <FormRow type="password" name="passwor" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member ?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
